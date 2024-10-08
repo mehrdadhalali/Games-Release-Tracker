804d077d772790ca3db230aba2c93a1416e0df22
Leon <leonjs02@protonmail.com>
2024-10-08 16:42:11 +0100

    Added function to return page source.

--- web_scraping/steam_scraper/scrape_steam.py
+++ web_scraping/steam_scraper/scrape_steam.py
@@ -1,4 +1,18 @@
 """Script to web scrape the Steam new-releases page for new games."""
 
+import requests as req
+
+STEAM_NEW_RELEASE_URL = "https://store.steampowered.com/search/?sort_by=Released_DESC&category1=998"
+TIMEOUT = 10
+
+
+def load_page_source(url: str) -> str:
+    """Returns the raw HTML string of a web-page's content."""
+    response = req.get(url, timeout=TIMEOUT)
+    if response.status_code != 200:
+        raise ConnectionError(f"Failed to connect to {url}")
+    return response.text
+
+
 if __name__ == "__main__":
     print("Hello world!")
