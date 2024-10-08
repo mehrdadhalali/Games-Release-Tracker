69f33b00e0be62c202a7b23d3e10b8e53b309126
Leon <leonjs02@protonmail.com>
2024-10-08 18:39:12 +0100

    Added test functions for scrape steam.

--- web_scraping/steam_scraper/test_scrape_steam.py
+++ web_scraping/steam_scraper/test_scrape_steam.py
@@ -1,7 +1,42 @@
+"""Tests for the scrape_steam.py file."""
 # pylint: skip-file
 
-from os import path
+from unittest import mock
+
+import pytest
+
+from scrape_steam import load_page_source
 
 
-def test_placeholder():
-    assert path.exists('README.md')
+@mock.patch('requests.get')
+def test_load_page_source_200_code_returns_string(mocked_get):
+    """Tests that a get request with a 200 status code returns a string of the correct value."""
+    mocked_response = mock.MagicMock()
+    mocked_response.status_code = 200
+    mocked_response.text = "<HTML></HTML>"
+    mocked_get.return_value = mocked_response
+    sample_url = "https://www.url.com"
+    content = load_page_source(sample_url)
+    assert isinstance(content, str)
+    assert content == mocked_response.text
+
+
+@mock.patch('requests.get')
+def test_load_page_source_404_raises_error(mocked_get):
+    """Tests that a get request with a 404 status code raises a connection error."""
+    mocked_response = mock.MagicMock()
+    mocked_response.status_code = 404
+    mocked_get.return_value = mocked_response
+    sample_url = "https://www.url.com"
+    with pytest.raises(ConnectionError):
+        load_page_source(sample_url)
+
+
+@mock.patch('requests.get')
+def test_load_page_source_calls_get(mocked_get):
+    """Tests that a function call to load page source calls requests.get once."""
+    mocked_get.return_value.status_code = 200
+    mocked_get.return_value.text = "Sample Page Content"
+    sample_url = "https://www.url.com"
+    load_page_source(sample_url)
+    assert mocked_get.assert_called_once
