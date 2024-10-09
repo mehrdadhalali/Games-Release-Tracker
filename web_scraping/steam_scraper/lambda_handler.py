42ab99848d78877fe60f587596c8514216a89b0e
Leon <leonjs02@protonmail.com>
2024-10-09 13:27:11 +0100

    Made the lambda status code match camelCase convention.

--- web_scraping/steam_scraper/lambda_handler.py
+++ web_scraping/steam_scraper/lambda_handler.py
@@ -10,15 +10,15 @@ from scrape_steam import collect_and_parse_games
 def lambda_handler(event, context):
     """Triggered with the lambda. Optional time argument can be
     passed in event: 'time'."""
+    time_arg = event.get('time', None)
     try:
-        time_arg = event.get('time', None)
         time_arg = datetime.fromisoformat(time_arg)
     except ValueError:
         time_arg = None
 
     listings = collect_and_parse_games(time_arg)
     return {
-        'status_code': 200,
+        'statusCode': 200,
         'body': {
             'data': listings
         }
