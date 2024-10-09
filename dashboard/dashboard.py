d3fef9de83b0ecf654ec7b61f71c3bf68ac15d78
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-10-09 12:25:07 +0100

    Dark theme and monospace

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,3 +3,10 @@ This is the main dashboard for the Games Tracker Streamlit application.
 It includes data visualisations on the information collected from Steam, GOG and Epic,
 which is stored in an AWS RDS database.
 """
+
+import streamlit as st
+
+# Page configuration
+st.set_page_config(layout="wide")
+
+st.title("Games Tracker")
