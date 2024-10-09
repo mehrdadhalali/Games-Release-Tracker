6b12b927d672cfc95d74a034f00551c594d6f247
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-10-09 13:50:12 +0100

    About title

--- dashboard/pages/about.py
+++ dashboard/pages/about.py
@@ -1,3 +1,10 @@
 """
 This is the "About" page of the dashboard, including information about the project and dashboard.
 """
+
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+# Title
+st.title("About Games Tracker")
