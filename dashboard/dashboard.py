0764fb27d391d96f20e033cd1047af4e7f630ebe
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-10-09 11:18:34 +0100

    Added pages structure to dashboard

---
+++ dashboard/dashboard.py
@@ -0,0 +1,5 @@
+"""
+This is the main dashboard for the Games Tracker Streamlit application.
+It includes data visualisations on the information collected from Steam, GOG and Epic,
+which is stored in an AWS RDS database.
+"""
