f4b5759d05b84b81f5c81b1a97380c8e53c73b09
Leon <leonjs02@protonmail.com>
2024-10-10 10:22:05 +0100

    Added some useful queries for report generation.

--- report/database_handler.py
+++ report/database_handler.py
@@ -25,3 +25,38 @@ def get_connection() -> connection:
 def get_cursor(conn: connection) -> cursor:
     """Returns a cursor from a database connection."""
     return conn.cursor(cursor_factory=RealDictCursor)
+
+
+def get_subscriber_info() -> dict:
+    """Returns a dictionary mapping subscriber name to subscriber email."""
+    select_str = "SELECT sub_name, sub_email FROM subscriber;"
+    with get_connection() as conn:
+        with get_cursor(conn) as cur:
+            cur.execute(select_str)
+            results = cur.fetchall()
+    return {x['sub_name']: x['sub_email'] for x in results}
+
+
+def get_top_genres() -> list[str]:
+    """Returns a list of the top three genres from the past week."""
+    select_str = """SELECT gr.genre_name, COUNT(*) AS genre_count
+    FROM genre AS gr JOIN game_genre_assignment AS gga 
+    ON gga.genre_id = gr.genre_id JOIN game AS g 
+    ON g.game_id = gga.game_id WHERE g.release_date > 
+    CURRENT_TIMESTAMP - INTERVAL '7 days' GROUP BY gr.genre_name
+    ORDER BY genre_count DESC LIMIT 3;"""
+    with get_connection() as conn:
+        with get_cursor(conn) as cur:
+            cur.execute(select_str)
+            results = cur.fetchall()
+    return [x['genre_name'] for x in results]
+
+
+def get_all_genres() -> list[str]:
+    """Returns a list of all genres in the database."""
+    with get_connection() as conn:
+        with get_cursor(conn) as cur:
+            cur.execute("SELECT * FROM genre;")
+            results = cur.fetchall()
+
+    return [x['genre_name'] for x in results]
