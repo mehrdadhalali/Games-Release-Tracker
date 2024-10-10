5b58445c214f29dca22d82b88cd56cf782350d3e
Leon <leonjs02@protonmail.com>
2024-10-10 22:04:06 +0100

    Added functions to extract information from API response str.

--- web_scraping/epic_games_scraper/extract_epic.py
+++ web_scraping/epic_games_scraper/extract_epic.py
@@ -29,3 +29,36 @@ def execute_query(q: gql) -> str:
     client = Client(transport=transport)
     result = client.execute(q)
     return dumps(result)
+
+
+def get_listings_from_json(json_str: str) -> list[dict]:
+    """Returns all game listing objects from the GraphQL response JSON."""
+    data = loads(json_str.strip())
+    return data['Catalog']['searchStore']['elements']
+
+
+def get_operating_systems(tags: list[dict]):
+    """Returns a list of operating systems from a list of tag dicts."""
+    return [tag['name'] for tag in tags if tag.get('groupName') == "platform"]
+
+
+def get_genres(tags: list[dict]):
+    """Returns a list of genre from a list of API tags"""
+    return [tag['name'] for tag in tags if tag.get('groupName') == "genre"]
+
+
+def get_features(tags: list[dict]):
+    """Returns all features from a list of tags"""
+    return [tag['name'] for tag in tags if tag.get('groupName') == "feature"]
+
+
+def format_release_date(release_date_str: str) -> str:
+    """Returns a string formatted from UTC to %d/%m/%Y."""
+    date_obj = datetime.fromisoformat(release_date_str.replace("Z", "+00:00"))
+    return date_obj.strftime("%d/%m/%y")
+
+
+def get_game_url(mappings: list[dict]) -> str:
+    """Returns the game's URL from a list of mappings."""
+    page_slug = mappings[0].get('pageSlug')
+    return f"https://store.epicgames.com/en-US/p/{page_slug}"
