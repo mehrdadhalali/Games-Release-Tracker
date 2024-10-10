0a9925e6f921d6bd1b18e75f77adbd0a97f52f69
Leon <leonjs02@protonmail.com>
2024-10-10 11:38:58 +0100

    Added queries to database handler for report gen.

--- report/database_handler.py
+++ report/database_handler.py
@@ -37,19 +37,20 @@ def get_subscriber_info() -> dict:
     return {x['sub_name']: x['sub_email'] for x in results}
 
 
-def get_top_genres() -> list[str]:
-    """Returns a list of the top three genres from the past week."""
+def get_genre_listing_count() -> dict:
+    """Returns a dictionary of mapping genre to listing
+     counts from the past week."""
     select_str = """SELECT gr.genre_name, COUNT(*) AS genre_count
     FROM genre AS gr JOIN game_genre_assignment AS gga 
     ON gga.genre_id = gr.genre_id JOIN game AS g 
     ON g.game_id = gga.game_id WHERE g.release_date > 
     CURRENT_TIMESTAMP - INTERVAL '7 days' GROUP BY gr.genre_name
-    ORDER BY genre_count DESC LIMIT 3;"""
+    ORDER BY genre_count DESC;"""
     with get_connection() as conn:
         with get_cursor(conn) as cur:
             cur.execute(select_str)
             results = cur.fetchall()
-    return [x['genre_name'] for x in results]
+    return {x['genre_name']: x['genre_count'] for x in results}
 
 
 def get_all_genres() -> list[str]:
@@ -62,7 +63,7 @@ def get_all_genres() -> list[str]:
     return [x['genre_name'] for x in results]
 
 
-def get_platform_popularity() -> dict:
+def get_platform_listing_count() -> dict:
     """Returns a dictionary mapping each platform to the number of new listings."""
     select_str = """SELECT p.platform_name, COUNT(gl.game_id) AS total_listings
     FROM game_listing AS gl
@@ -94,3 +95,8 @@ def get_platform_average_price() -> dict:
             results = cur.fetchall()
 
     return {x['platform_name']: x['average_price'] for x in results}
+
+
+def get_free_to_play() -> list[dict]:
+    """Returns a list of game objects which are currently
+    free to play."""
