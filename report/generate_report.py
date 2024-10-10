77ad6d99dc1db51423240226d60214e7ef333a80
Leon <leonjs02@protonmail.com>
2024-10-10 14:18:17 +0100

    Added script to build story and generate PDF file.

--- report/generate_report.py
+++ report/generate_report.py
@@ -5,26 +5,35 @@ from datetime import datetime, timedelta
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.platypus import Spacer
 
 
 def get_table_style() -> TableStyle:
-    """Returns the table style used for the PDF document."""
+    """Returns a table style with a dark theme."""
     return TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#7f5ace")),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.HexColor("#2E2E2E")),
+        ('TEXTCOLOR', (0, 1), (-1, -1), colors.whitesmoke),
+        ('GRID', (0, 0), (-1, -1), 1, colors.HexColor("#6A0DAD")),
     ])
 
 
-def create_heading(text: str, style: str) -> Paragraph:
-    """Creates a heading paragraph."""
-    styles = getSampleStyleSheet()
-    return Paragraph(text, styles[style])
+def create_heading(text: str) -> Paragraph:
+    """Creates a heading paragraph with a dark theme."""
+    heading_style = ParagraphStyle(
+        name='HeadingStyle',
+        fontName='Courier-Bold',
+        fontSize=12,
+        spaceAfter=12,
+        alignment=0,
+        textColor=colors.black,
+    )
+    return Paragraph(text, heading_style)
 
 
 def create_genre_table(genre_data: dict) -> Table:
@@ -57,29 +66,65 @@ def create_releases_table(platform_releases: dict) -> Table:
     return releases_table
 
 
-def generate_summary_report_pdf(total_games: int, total_platforms: int, genre_data: dict, platform_pricing: dict, platform_releases: dict) -> bytes:
-    """Generates the structure of the attachment report from the given data."""
-    today = datetime.now()
-    seven_days_ago = today - timedelta(days=7)
-    date_range = f"{seven_days_ago.strftime(
-        '%d/%m/%Y')} - {today.strftime('%d/%m/%Y')}"
+def create_free_to_play_table(free_to_play: dict) -> Table:
+    """Creates a table for the free-to-play data."""
+    free_table_data = [['Game Title', 'Platform']]
+    for title, platform in free_to_play.items():
+        free_table_data.append([title, platform])
+    free_table = Table(free_table_data)
+    free_table.setStyle(get_table_style())
+    return free_table
 
+
+def generate_summary_report_pdf(total_games: int, total_platforms: int, genre_data: dict,
+                                platform_pricing: dict, platform_releases: dict,
+                                free_games: dict) -> bytes:
+    """Generates the structure of the attachment report from the given data."""
+    date_range = get_date_range()
     pdf_buffer = BytesIO()
     pdf = SimpleDocTemplate(pdf_buffer, pagesize=letter)
-    story = []
-    story.append(create_heading(f"Gaming Trends: {date_range}", 'Heading1'))
+    story = build_story(total_games, total_platforms, genre_data,
+                        platform_pricing, platform_releases, free_games, date_range)
 
-    story.append(create_heading(f'This week: <b>{
-                 total_games}</b> different games were released across <b>{total_platforms}</b> platforms.', 'Title'))
-    story.append(create_heading('Breakdown by Genre:', 'Heading2'))
-    story.append(create_genre_table(genre_data))
-
-    story.append(create_heading('Average Price Per Platform:', 'Heading2'))
-    story.append(create_pricing_table(platform_pricing))
-
-    story.append(create_heading('Total Releases by Platform:', 'Heading2'))
-    story.append(create_releases_table(platform_releases))
     pdf.build(story)
     pdf_bytes = pdf_buffer.getvalue()
     pdf_buffer.close()
     return pdf_bytes
+
+
+def get_date_range() -> str:
+    """Returns the date range for the report."""
+    today = datetime.now()
+    seven_days_ago = today - timedelta(days=7)
+    return f"{seven_days_ago.strftime('%d/%m/%Y')} - {today.strftime('%d/%m/%Y')}"
+
+
+def build_story(total_games: int, total_platforms: int, genre_data: dict,
+                platform_pricing: dict, platform_releases: dict,
+                free_games: dict, date_range: str) -> list:
+    """Builds the 'story' for creating the PDF document.
+    Stories are incrementally appended to, and built to a PDF."""
+    story = []
+    spacer = Spacer(2, 20)
+    story.append(create_heading(f"Gaming Trends: {date_range}"))
+    story.append(create_heading(f'''This week: <b>{total_games}</b> different games
+                                     were released across <b>{total_platforms}</b> platforms.'''))
+    story.append(spacer)
+    story.append(create_heading('Breakdown by Genre:'))
+    story.append(create_genre_table(genre_data))
+    story.append(spacer)
+    story.append(create_heading('Average Price Per Platform:'))
+    story.append(create_pricing_table(platform_pricing))
+    story.append(spacer)
+    story.append(create_heading('Total Releases by Platform:'))
+    story.append(create_releases_table(platform_releases))
+    story.append(spacer)
+    story.append(create_heading('Free to Play Games:'))
+    story.append(create_free_to_play_table(free_games))
+    return story
+
+
+def write_bytes_to_local_file(pdf_bytes: bytes):
+    """Temporary function: writes PDF locally."""
+    with open('report.pdf', 'wb') as file:
+        file.write(pdf_bytes)
