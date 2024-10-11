16292f64d52ecca22c252083ac7416f696aabe30
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-10-11 15:38:00 +0100

    Email input applied

--- dashboard/pages/Subscribe.py
+++ dashboard/pages/Subscribe.py
@@ -3,7 +3,12 @@ This page includes the functionality for users to subscribe
 and unsubscribe to email updates about game releases.
 """
 
+import re
+
 import streamlit as st
+
+# Regex for validating email
+email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
 st.set_page_config(layout="wide")
 
@@ -15,7 +20,7 @@ st.write("""Subscribe to stay updated! Select the types of content
 
 st.divider()
 
-cols = st.columns([1.1, 2, 1])
+cols = st.columns([1.1, 2, 0.8])
 with cols[0]:
     st.write("Receive our weekly report:")
 with cols[1]:
@@ -23,8 +28,7 @@ with cols[1]:
 with cols[2]:
     st.write("Include NSFW games:")
 
-cols = st.columns([1.1, 1, 1, 1])
-
+cols = st.columns([1.1, 1, 1, 0.8])
 with cols[0]:
     weekly_report = st.checkbox("Weekly Report")
 
@@ -48,3 +52,28 @@ with cols[2]:
 
 with cols[3]:
     email_nsfw = st.checkbox("Include NSFW")
+
+cols = st.columns([1, 2, 1])
+with cols[1]:
+    st.write(" <br> ", unsafe_allow_html=True)
+
+    email = st.text_input("Enter your email:")
+
+    if st.button("Subscribe"):
+        if re.match(email_pattern, email):
+            st.success(f"You are subscribed with {email}. Bosh!")
+        else:
+            st.warning("Please enter a valid email address.")
+
+st.divider()
+
+cols = st.columns([1, 2, 1])
+with cols[1]:
+    with st.expander("Unsubscribe", expanded=False):
+        unsubscribe_email = st.text_input(
+            "Enter your email:", key="unsubscribe")
+        if st.button("Unsubscribe"):
+            if re.match(email_pattern, unsubscribe_email):
+                st.success(f"You have unsubscribed {unsubscribe_email}.")
+            else:
+                st.warning("Please enter a valid email address.")
